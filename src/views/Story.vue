6e098209c8645a76781a6cd0baeac0753da26e37
yangxiaoping <2878847069@qq.com>
2024-05-30 19:04:38 +0800

    feat: 添加story路由

---
+++ src/views/Story.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Story</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
