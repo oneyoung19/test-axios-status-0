a2c32e578928d29f681fe51f1267f37f636c29bf
yangxiaoping <2878847069@qq.com>
2024-05-31 11:19:00 +0800

    feat: 添加vConsole

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VConsole from 'vconsole'
+
+// eslint-disable-next-line
+new VConsole()
 
 Vue.config.productionTip = false
 
